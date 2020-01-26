ff9456230368453ae43215f9eedaa4a46108e01f
makesmart <github@makesmart.net>
2020-01-26 14:13:50 +0100

    Update new.sh

--- data/project/new.sh
+++ data/project/new.sh
@@ -13,5 +13,9 @@ read project_name
 #Temporäre Datei erstellen
 echo "$project_name" > cloud/data/_temp/name
 
+#Temporäre Datei erstellen
+echo $(hostname) > cloud/data/_temp/hostname
+echo $(whoami) > cloud/data/_temp/logged_in_user
+
 #../mysql/start.sh aufrufen
 sudo bash cloud/data/mysql/start.sh
